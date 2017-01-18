285888b5cb6200f4c23bc733313712654e16ab74
FelixKingen <Felix.hell@hotmail.com>
2017-01-18 22:24:42 +0100

    Initial commit

---
+++ HelloWorld/PrintHW.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class PrintHW
+    {
+        public static void PrintThis()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+        
+    }
+}
